0fc0bb8ebcb3829fec04057a054f33fcdcbe7a36
akdeb <deb2@illinois.edu>
2024-06-27 19:22:29 +0100

    update insights

--- frontend-ts/lib/azureOpenai.ts
+++ frontend-ts/lib/azureOpenai.ts
@@ -5,35 +5,35 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 export const generateSuggestion = async (
-  cardData: string,
-  barData: any,
-  lineData: any,
-  pieData: any
+    cardData: string,
+    barData: any,
+    lineData: any,
+    pieData: any
 ) => {
-  // You will need to set these environment variables or edit the following values
-  const endpoint = process.env["AZURE_OPENAI_ENDPOINT"];
-  const apiKey = process.env["AZURE_OPENAI_API_KEY"];
-  const apiVersion = "2024-02-01"; //"2024-02-01"
-  const deployment = process.env["MODEL_NAME"] || "gpt-4o";
+    // You will need to set these environment variables or edit the following values
+    const endpoint = process.env["AZURE_OPENAI_ENDPOINT"];
+    const apiKey = process.env["AZURE_OPENAI_API_KEY"];
+    const apiVersion = "2024-02-01"; //"2024-02-01"
+    const deployment = process.env["MODEL_NAME"] || "gpt-4o";
 
-  // convert cardData to string
-  console.log("cardData", cardData);
+    // convert cardData to string
+    console.log("cardData", cardData);
 
-  const client = new AzureOpenAI({
-    endpoint,
-    apiKey,
-    apiVersion,
-    deployment,
-  });
-  const result = await client.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: `You are an assistant who helps parents provide insight based on children's emotional data.`,
-      },
-      {
-        role: "user",
-        content: `Please provide a 50-word of suggestion of the below data:
+    const client = new AzureOpenAI({
+        endpoint,
+        apiKey,
+        apiVersion,
+        deployment,
+    });
+    const result = await client.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `You are an assistant who helps parents provide insight based on children's emotional data.`,
+            },
+            {
+                role: "user",
+                content: `Please provide a 50-word of suggestion of the below data:
 
         Main Emotions & Significant Emotional Shifts with today and yesterday data:\n
         ${cardData}
@@ -46,10 +46,9 @@ export const generateSuggestion = async (
         
         Current top 10 Emotions Breakdown 
         ${barData}`,
-      },
-    ],
-    model: "",
-  });
+            },
+        ],
+    });
 
-  return result.choices[0].message.content;
+    return result.choices[0].message.content;
 };
