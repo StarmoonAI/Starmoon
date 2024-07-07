5d894d3103f2b4e4538967396a8cd9ad47513886
Joey Xiong <junruxiong@gmail.com>
2024-07-07 01:57:11 +0100

    add voice tone

--- backend-py/app/api/endpoints/starmoon.py
+++ backend-py/app/api/endpoints/starmoon.py
@@ -40,14 +40,43 @@ client = Clients()
 speech_config = SpeechConfig(
     subscription="d9e1868008cf477eb9cad5ddca6e4994", region="eastus"
 )
+speech_config.set_property(
+    property_id=speechsdk.PropertyId.SpeechServiceResponse_RequestSentenceBoundary,
+    value="true",
+)
 
 
-async def speech_stream_response(text: str, websocket: WebSocket):
+def voice_systhesizer(
+    text: str,
+    language: str = "en-US",
+    voice_name: str = "en-US-AvaMultilingualNeural",
+    emotion: str = "",
+    emotion_degree: float = 0,  # default
+    emotion_role: str = None,
+    rate: float = 0,
+    pitch: float = 0,
+):
+    # https://learn.microsoft.com/en-us/azure/ai-services/speech-service/speech-synthesis-markup-voice
+
+    ssml = f"""<speak version='1.0' xml:lang="{language}" xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='http://www.w3.org/2001/mstts'>
+    <voice name="{voice_name}">
+        <mstts:express-as style="{emotion}" styledegree="{emotion_degree}">
+            <prosody rate="{rate}%" pitch="{pitch}%">
+                {text}
+            </prosody>
+        </mstts:express-as>
+    </voice>
+    </speak>"""
+
+    return ssml
+
+
+async def speech_stream_response(ssml: str, websocket: WebSocket):
     speech_synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config, audio_config=None
     )
 
-    result = speech_synthesizer.start_speaking_text_async(text).get()
+    result = speech_synthesizer.speak_ssml_async(ssml).get()
     audio_data_stream = speechsdk.AudioDataStream(result)
     audio_buffer = bytes(32000)
     filled_size = audio_data_stream.read_data(audio_buffer)
@@ -90,7 +119,14 @@ async def speech_stream_response_azure(transcription: dict, websocket: WebSocket
                                 }
                             )
                             await asyncio.sleep(0)
-                            await speech_stream_response(sentence.strip(), websocket)
+                            text_tone = voice_systhesizer(
+                                text=sentence.strip(),
+                                voice_name="en-US-AriaNeural",
+                                emotion="whispering",
+                                emotion_degree=1,
+                                rate=-10,
+                            )
+                            await speech_stream_response(text_tone, websocket)
                     # Keep the last (possibly incomplete) sentence
                     accumulated_text = sentences[-1]
     # Send any remaining text
@@ -99,7 +135,14 @@ async def speech_stream_response_azure(transcription: dict, websocket: WebSocket
             {"response": accumulated_text.strip(), "is_running": True}
         )
         await asyncio.sleep(0)
-        await speech_stream_response(accumulated_text.strip(), websocket)
+        text_tone = voice_systhesizer(
+            accumulated_text.strip(),
+            voice_name="en-US-AriaNeural",
+            emotion="whispering",
+            emotion_degree=1,
+            rate=-10,
+        )
+        await speech_stream_response(text_tone, websocket)
         await asyncio.sleep(0)
     await websocket.send_json({"response": "", "is_running": False})
 
