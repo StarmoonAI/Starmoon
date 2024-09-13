55a3e072466abe0586d2c5cbea74306bd9f6e3f3
akdeb <deb2@illinois.edu>
2024-09-13 19:22:14 +0100

    works

--- frontend/app/components/playground/PlaygroundComponent.tsx
+++ frontend/app/components/playground/PlaygroundComponent.tsx
@@ -19,236 +19,235 @@ import CreditsRemaining from "../CreditsRemaining";
 import AddCreditsModal from "../Upsell/AddCreditsModal";
 
 interface PlaygroundProps {
-  selectedUser: IUser;
-  allToys: IToy[];
-  allPersonalities: IPersonality[];
+    selectedUser: IUser;
+    allToys: IToy[];
+    allPersonalities: IPersonality[];
 }
 
 const Playground: React.FC<PlaygroundProps> = ({
-  selectedUser,
-  allToys,
-  allPersonalities,
+    selectedUser,
+    allToys,
+    allPersonalities,
 }) => {
-  const supabase = createClient();
+    const supabase = createClient();
 
-  const {
-    messageHistory,
-    emotionDictionary,
-    connectionStatus,
-    microphoneStream,
-    audioBuffer,
-    handleClickOpenConnection,
-    handleClickInterrupt,
-    handleClickCloseConnection,
-    muteMicrophone,
-    unmuteMicrophone,
-    isMuted,
-  } = useWebSocketHandler(selectedUser);
+    const {
+        messageHistory,
+        emotionDictionary,
+        connectionStatus,
+        microphoneStream,
+        audioBuffer,
+        handleClickOpenConnection,
+        handleClickInterrupt,
+        handleClickCloseConnection,
+        muteMicrophone,
+        unmuteMicrophone,
+        isMuted,
+    } = useWebSocketHandler(selectedUser);
 
-  const selectedToy = selectedUser.toy!;
-  const selectedPersonality = selectedUser.personality!;
+    const selectedToy = selectedUser.toy!;
+    const selectedPersonality = selectedUser.personality!;
 
-  // Debounced function to update the user on the server
-  const debouncedUpdateUser = _.debounce(
-    async ({
-      personality_id,
-      toy_id,
-    }: {
-      personality_id: string;
-      toy_id: string;
-    }) => {
-      await updateUser(
-        supabase,
-        { personality_id, toy_id },
-        selectedUser.user_id
-      );
-    },
-    1000
-  ); // Adjust the debounce delay as needed
+    // Debounced function to update the user on the server
+    const debouncedUpdateUser = _.debounce(
+        async ({
+            personality_id,
+            toy_id,
+        }: {
+            personality_id: string;
+            toy_id: string;
+        }) => {
+            await updateUser(
+                supabase,
+                { personality_id, toy_id },
+                selectedUser.user_id
+            );
+        },
+        1000
+    ); // Adjust the debounce delay as needed
 
-  const [userState, setUserState] = useState<IUser>(selectedUser);
-  const creditsRemaining = getCreditsRemaining(userState);
-  // const ref: any = useRef<ComponentRef<typeof Messages> | null>(null);
+    const [userState, setUserState] = useState<IUser>(selectedUser);
+    const creditsRemaining = getCreditsRemaining(userState);
+    // const ref: any = useRef<ComponentRef<typeof Messages> | null>(null);
 
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  const [isScrolledToBottom, setIsScrolledToBottom] = useState(true);
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const [isScrolledToBottom, setIsScrolledToBottom] = useState(true);
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
 
-  const isSelectDisabled = connectionStatus === "Open";
+    const isSelectDisabled = connectionStatus === "Open";
 
-  const handleScroll = () => {
-    if (scrollContainerRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } =
-        scrollContainerRef.current;
-      const isAtBottom = scrollTop + clientHeight >= scrollHeight - 10; // 10px threshold
-      setIsScrolledToBottom(isAtBottom);
-    }
-  };
+    const handleScroll = () => {
+        if (scrollContainerRef.current) {
+            const { scrollTop, scrollHeight, clientHeight } =
+                scrollContainerRef.current;
+            const isAtBottom = scrollTop + clientHeight >= scrollHeight - 10; // 10px threshold
+            setIsScrolledToBottom(isAtBottom);
+        }
+    };
 
-  useEffect(() => {
-    if (isScrolledToBottom) {
-      scrollToBottom();
-    }
-  }, [messageHistory, emotionDictionary, isScrolledToBottom]);
+    useEffect(() => {
+        if (isScrolledToBottom) {
+            scrollToBottom();
+        }
+    }, [messageHistory, emotionDictionary, isScrolledToBottom]);
 
-  useEffect(() => {
-    const scrollContainer = scrollContainerRef.current;
-    if (scrollContainer) {
-      scrollContainer.addEventListener("scroll", handleScroll);
-      return () => scrollContainer.removeEventListener("scroll", handleScroll);
-    }
-  }, []);
+    useEffect(() => {
+        const scrollContainer = scrollContainerRef.current;
+        if (scrollContainer) {
+            scrollContainer.addEventListener("scroll", handleScroll);
+            return () =>
+                scrollContainer.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
 
-  const [personalityState, setPersonalityState] =
-    useState<IPersonality>(selectedPersonality);
-  const [toyState, setToyState] = useState<IToy>(selectedToy);
+    const [personalityState, setPersonalityState] =
+        useState<IPersonality>(selectedPersonality);
+    const [toyState, setToyState] = useState<IToy>(selectedToy);
 
-  const onPersonalityPicked = (personalitySelected: IPersonality) => {
-    // Instantaneously update the state variable
-    setPersonalityState(personalitySelected);
+    const onPersonalityPicked = (personalitySelected: IPersonality) => {
+        // Instantaneously update the state variable
+        setPersonalityState(personalitySelected);
 
-    // Debounce the server update
-    debouncedUpdateUser({
-      personality_id: personalitySelected.personality_id,
-      toy_id: toyState.toy_id,
-    });
-  };
+        // Debounce the server update
+        debouncedUpdateUser({
+            personality_id: personalitySelected.personality_id,
+            toy_id: toyState.toy_id,
+        });
+    };
 
-  const onVoicePicked = (toySelected: IToy) => {
-    // Instantaneously update the state variable
-    setToyState(toySelected);
+    const onVoicePicked = (toySelected: IToy) => {
+        // Instantaneously update the state variable
+        setToyState(toySelected);
 
-    // Debounce the server update
-    debouncedUpdateUser({
-      personality_id: personalityState.personality_id,
-      toy_id: toySelected.toy_id,
-    });
-  };
+        // Debounce the server update
+        debouncedUpdateUser({
+            personality_id: personalityState.personality_id,
+            toy_id: toySelected.toy_id,
+        });
+    };
 
-  console.log("personalityState!!!!", personalityState);
+    console.log("personalityState!!!!", personalityState);
 
-  return (
-    <div className="flex flex-col">
-      <div className="flex flex-col w-full gap-2">
-        <h1 className="text-3xl font-normal">Playground</h1>
-        <CreditsRemaining user={userState} />
-        {messageHistory.length === 0 ? (
-          <div className="flex flex-col w-full justify-center gap-2">
-            <div className="flex flex-col max-h-[300px] items-start gap-2 my-4 transition-colors duration-200 ease-in-out">
-              <div className="flex flex-row items-start gap-8">
-                <PickVoice
-                  onVoicePicked={onVoicePicked}
-                  allToys={allToys}
-                  toyState={toyState}
-                  isDisabled={isSelectDisabled}
-                />
-                <PickPersonality
-                  onPersonalityPicked={onPersonalityPicked}
-                  allPersonalities={allPersonalities}
-                  personalityState={personalityState}
-                  toyState={toyState}
-                  isDisabled={isSelectDisabled}
-                />
-              </div>
-              <div className="flex flex-row items-center self-center">
-                <div className="max-w-[200px] transition-transform duration-300 ease-in-out scale-90 hover:scale-100">
-                  <Image
-                    // src={getAssistantAvatar(toyState.image_src!)}
-                    src={`/personality/${toyState?.image_src}_${personalityState.title.toLowerCase().replace(/\s+/g, "_")}.png`}
-                    width={200}
-                    height={200}
-                    alt={toyState.name}
-                    className="w-full h-auto" // Make image responsive within container
-                  />
-                </div>
-                {/* <MoonStar fill="#4b5563" className="text-gray-600" />
-                <div className="max-w-[150px]  transition-transform duration-300 ease-in-out scale-90 hover:scale-100">
-                  <Image
-                    src={getUserAvatar(userState.email)}
-                    width={200}
-                    height={200}
-                    alt={userState.supervisee_name}
-                    className="w-full h-auto" // Make image responsive within container
-                  />
-                </div> */}
-              </div>
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-col w-full gap-2">
+                <h1 className="text-3xl font-normal">Playground</h1>
+                <CreditsRemaining user={userState} />
+                {messageHistory.length === 0 ? (
+                    <div className="flex flex-col w-full justify-center gap-2">
+                        <div className="flex flex-col max-h-[300px] items-start gap-2 my-4 transition-colors duration-200 ease-in-out">
+                            <div className="flex flex-row items-start gap-8">
+                                <PickVoice
+                                    onVoicePicked={onVoicePicked}
+                                    allToys={allToys}
+                                    toyState={toyState}
+                                    isDisabled={isSelectDisabled}
+                                />
+                                <PickPersonality
+                                    onPersonalityPicked={onPersonalityPicked}
+                                    allPersonalities={allPersonalities}
+                                    personalityState={personalityState}
+                                    toyState={toyState}
+                                    isDisabled={isSelectDisabled}
+                                />
+                            </div>
+                            <div className="flex flex-row items-center self-center">
+                                <div className="max-w-[200px] transition-transform duration-300 ease-in-out scale-90 hover:scale-100">
+                                    <Image
+                                        // src={getAssistantAvatar(toyState.image_src!)}
+                                        src={`/personality/${toyState?.image_src}_${personalityState.title.toLowerCase().replace(/\s+/g, "_")}.png`}
+                                        width={200}
+                                        height={200}
+                                        alt={toyState.name}
+                                        className="w-full h-auto" // Make image responsive within container
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ) : null}
+
+                {connectionStatus !== "Open" ? (
+                    <div className="w-full flex flex-col gap-8 items-center justify-center">
+                        <AnimatePresence>
+                            <motion.div
+                                initial="initial"
+                                animate="enter"
+                                exit="exit"
+                                variants={{
+                                    initial: { opacity: 0 },
+                                    enter: { opacity: 1 },
+                                    exit: { opacity: 0 },
+                                }}
+                            >
+                                {creditsRemaining <= 0 ? (
+                                    <AddCreditsModal>
+                                        <Button
+                                            className={
+                                                "z-50 flex items-center gap-1.5"
+                                            }
+                                            size="sm"
+                                            variant={"upsell_primary"}
+                                        >
+                                            <Sparkles
+                                                size={16}
+                                                strokeWidth={3}
+                                                stroke={"currentColor"}
+                                            />
+                                            <span className="text-md font-semibold">
+                                                Add credits
+                                            </span>
+                                        </Button>
+                                    </AddCreditsModal>
+                                ) : (
+                                    <Button
+                                        disabled={!selectedUser}
+                                        className={
+                                            "z-50 flex items-center gap-1.5 rounded-full"
+                                        }
+                                        onClick={handleClickOpenConnection}
+                                        size="sm"
+                                    >
+                                        <MoonStar
+                                            size={16}
+                                            strokeWidth={3}
+                                            stroke={"currentColor"}
+                                        />
+                                        <span className="text-md font-semibold">
+                                            Play
+                                        </span>
+                                    </Button>
+                                )}
+                            </motion.div>
+                        </AnimatePresence>
+                    </div>
+                ) : null}
             </div>
-          </div>
-        ) : null}
 
-        {connectionStatus !== "Open" ? (
-          <div className="w-full flex flex-col gap-8 items-center justify-center">
-            <AnimatePresence>
-              <motion.div
-                initial="initial"
-                animate="enter"
-                exit="exit"
-                variants={{
-                  initial: { opacity: 0 },
-                  enter: { opacity: 1 },
-                  exit: { opacity: 0 },
-                }}
-              >
-                {creditsRemaining <= 0 ? (
-                  <AddCreditsModal>
-                    <Button
-                      className={"z-50 flex items-center gap-1.5"}
-                      size="sm"
-                      variant={"upsell_primary"}
-                    >
-                      <Sparkles
-                        size={16}
-                        strokeWidth={3}
-                        stroke={"currentColor"}
-                      />
-                      <span className="text-md font-semibold">Add credits</span>
-                    </Button>
-                  </AddCreditsModal>
-                ) : (
-                  <Button
-                    disabled={!selectedUser}
-                    className={"z-50 flex items-center gap-1.5 rounded-full"}
-                    onClick={handleClickOpenConnection}
-                    size="sm"
-                  >
-                    <MoonStar
-                      size={16}
-                      strokeWidth={3}
-                      stroke={"currentColor"}
-                    />
-                    <span className="text-md font-semibold">Play</span>
-                  </Button>
-                )}
-              </motion.div>
-            </AnimatePresence>
-          </div>
-        ) : null}
-      </div>
-
-      <Messages
-        messageHistory={messageHistory}
-        selectedUser={selectedUser}
-        selectedToy={toyState}
-        emotionDictionary={emotionDictionary}
-        handleScroll={handleScroll}
-        isScrolledToBottom={isScrolledToBottom}
-      />
-      <ControlPanel
-        connectionStatus={connectionStatus}
-        isMuted={isMuted}
-        muteMicrophone={muteMicrophone}
-        unmuteMicrophone={unmuteMicrophone}
-        handleClickInterrupt={handleClickInterrupt}
-        handleClickCloseConnection={handleClickCloseConnection}
-        microphoneStream={microphoneStream}
-        audioBuffer={audioBuffer}
-      />
-    </div>
-  );
+            <Messages
+                messageHistory={messageHistory}
+                selectedUser={selectedUser}
+                selectedToy={toyState}
+                emotionDictionary={emotionDictionary}
+                handleScroll={handleScroll}
+                isScrolledToBottom={isScrolledToBottom}
+            />
+            <ControlPanel
+                connectionStatus={connectionStatus}
+                isMuted={isMuted}
+                muteMicrophone={muteMicrophone}
+                unmuteMicrophone={unmuteMicrophone}
+                handleClickInterrupt={handleClickInterrupt}
+                handleClickCloseConnection={handleClickCloseConnection}
+                microphoneStream={microphoneStream}
+                audioBuffer={audioBuffer}
+            />
+        </div>
+    );
 };
 
 export default Playground;
