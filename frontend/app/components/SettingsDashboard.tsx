b54218bb4f7a45a5d96ec0c98a51e871f825b30e
akdeb <deb2@illinois.edu>
2024-09-13 19:43:07 +0100

    updated settings

--- frontend/app/components/SettingsDashboard.tsx
+++ frontend/app/components/SettingsDashboard.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -26,293 +26,205 @@ import React from "react";
 import CreditsRemaining from "./CreditsRemaining";
 import { generateStarmoonAuthKey } from "../actions";
 import AuthTokenModal from "./AuthTokenModal";
+import {
+    userFormAgeDescription,
+    userFormAgeLabel,
+    userFormPersonaLabel,
+} from "@/lib/data";
 
 interface SettingsDashboardProps {
-  selectedUser: IUser;
+    selectedUser: IUser;
 }
 
 export const settingsDashboardSchema = z.object({
-  supervisee_name: z.string().min(1).max(50),
-  supervisee_age: z.number().min(1).max(18),
-  supervisee_persona: z.string().max(500).optional(),
-  modules: z
-    .array(z.enum(["math", "science", "spelling", "general_trivia"]))
-    .refine((value) => value.some((item) => item), {
-      message: "You have to select at least one item.",
-    }),
+    supervisee_name: z.string().min(1).max(50),
+    supervisee_age: z.number().min(1).max(18),
+    supervisee_persona: z.string().max(500).optional(),
+    modules: z
+        .array(z.enum(["math", "science", "spelling", "general_trivia"]))
+        .refine((value) => value.some((item) => item), {
+            message: "You have to select at least one item.",
+        }),
 });
 
 const learningModules = [
-  {
-    id: "math",
-    label: "Math",
-  },
-  {
-    id: "science",
-    label: "Science",
-  },
-  {
-    id: "spelling",
-    label: "Spelling",
-  },
-  {
-    id: "general_trivia",
-    label: "General trivia",
-  },
+    {
+        id: "math",
+        label: "Math",
+    },
+    {
+        id: "science",
+        label: "Science",
+    },
+    {
+        id: "spelling",
+        label: "Spelling",
+    },
+    {
+        id: "general_trivia",
+        label: "General trivia",
+    },
 ] as { id: Module; label: string }[];
 
 export type SettingsFormInput = z.infer<typeof settingsDashboardSchema>;
 
 const SettingsDashboard: React.FC<SettingsDashboardProps> = ({
-  selectedUser,
+    selectedUser,
 }) => {
-  const supabase = createClient();
-  const { toast } = useToast();
-  const form = useForm<SettingsFormInput>({
-    defaultValues: {
-      supervisee_name: selectedUser?.supervisee_name ?? "",
-      supervisee_age: selectedUser?.supervisee_age ?? 0,
-      supervisee_persona: selectedUser?.supervisee_persona ?? "",
-      modules: selectedUser?.modules ?? [],
-    },
-  });
-
-  async function onSubmit(values: z.infer<typeof settingsDashboardSchema>) {
-    await updateUser(supabase, values, selectedUser!.user_id);
-    toast({
-      description: "Your prefereces have been saved.",
+    const supabase = createClient();
+    const { toast } = useToast();
+    const form = useForm<SettingsFormInput>({
+        defaultValues: {
+            supervisee_name: selectedUser?.supervisee_name ?? "",
+            supervisee_age: selectedUser?.supervisee_age ?? 0,
+            supervisee_persona: selectedUser?.supervisee_persona ?? "",
+            modules: selectedUser?.modules ?? [],
+        },
     });
-  }
 
-  return (
-    <div className="overflow-hidden w-full flex-auto flex flex-col pl-1">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-8 mb-4"
-        >
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-4 items-center">
-              <h1 className="text-3xl font-normal">Settings</h1>
-              <div className="flex flex-row gap-2 justify-between items-center">
-                <Button variant="default" size="sm" type="submit">
-                  Save
-                </Button>
-              </div>
-            </div>
-            <CreditsRemaining user={selectedUser} />
-          </div>
-          <FormField
-            control={form.control}
-            name="supervisee_name"
-            render={({ field }) => (
-              <FormItem className="w-full rounded-md">
-                <FormLabel className="flex flex-row gap-4 items-center">
-                  {"The user's name"}
-                </FormLabel>
-                {/* <FormDescription>
+    async function onSubmit(values: z.infer<typeof settingsDashboardSchema>) {
+        await updateUser(supabase, values, selectedUser!.user_id);
+        toast({
+            description: "Your prefereces have been saved.",
+        });
+    }
+
+    return (
+        <div className="overflow-hidden w-full flex-auto flex flex-col pl-1">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col gap-8 mb-4"
+                >
+                    <div className="flex flex-col gap-2">
+                        <div className="flex flex-row gap-4 items-center">
+                            <h1 className="text-3xl font-normal">Settings</h1>
+                            <div className="flex flex-row gap-2 justify-between items-center">
+                                <Button
+                                    variant="default"
+                                    size="sm"
+                                    type="submit"
+                                >
+                                    Save
+                                </Button>
+                            </div>
+                        </div>
+                        <CreditsRemaining user={selectedUser} />
+                    </div>
+                    <FormField
+                        control={form.control}
+                        name="supervisee_name"
+                        render={({ field }) => (
+                            <FormItem className="w-full rounded-md">
+                                <FormLabel className="flex flex-row gap-4 items-center">
+                                    {"Your name"}
+                                </FormLabel>
+                                {/* <FormDescription>
                             Give your newsletter a name that describes its
                             content.
                         </FormDescription> */}
-                <FormControl>
-                  <Input
-                    // autoFocus
-                    required
-                    placeholder="e.g. John Doe"
-                    {...field}
-                    className="max-w-screen-sm h-10 bg-white"
-                    autoComplete="on"
-                    style={{
-                      fontSize: 16,
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* for child age */}
-          <FormField
-            control={form.control}
-            name="supervisee_age"
-            render={({ field }) => (
-              <FormItem className="w-full rounded-md">
-                <FormLabel className="flex flex-row gap-4 items-center">
-                  {"The user's age"}
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    // autoFocus
-                    required
-                    placeholder="e.g. 8"
-                    {...field}
-                    className="max-w-screen-sm h-10 bg-white"
-                    autoComplete="on"
-                    style={{
-                      fontSize: 16,
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="supervisee_persona"
-            render={({ field }) => (
-              <FormItem className="w-full rounded-md">
-                <FormLabel className="flex flex-row gap-4 items-center">
-                  Briefly describe the user&apos;s interests, personality, and
-                  learning style
-                </FormLabel>
-                <FormControl>
-                  <Textarea
-                    rows={4}
-                    placeholder="e.g. I would like the AI to be friendly and encouraging, and to use positive reinforcement to help my child learn."
-                    {...field}
-                    className="max-w-screen-sm bg-white"
-                    autoComplete="on"
-                    style={{
-                      fontSize: 16,
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* <FormField
-            control={form.control}
-            name="supervisee_persona"
-            render={({ field }) => (
-              <FormItem className="w-full rounded-md">
-                <FormLabel className="flex flex-row gap-4 items-center">
-                  Banned topics
-                </FormLabel>
-                <FormControl>
-                  <Textarea
-                    rows={4}
-                    placeholder="e.g. I would like the AI to be friendly and encouraging, and to use positive reinforcement to help my child learn."
-                    {...field}
-                    className="max-w-screen-sm bg-white"
-                    autoComplete="on"
-                    style={{
-                      fontSize: 16,
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
-          {/* <FormField
-                        control={form.control}
-                        name="modules"
-                        render={() => (
-                            <FormItem>
-                                <div className="mb-4">
-                                    <FormLabel className="text-base">
-                                        Learning modules
-                                    </FormLabel>
-                                    <FormDescription>
-                                        Select your learning modules
-                                    </FormDescription>
-                                </div>
-                                {learningModules.map((item) => (
-                                    <FormField
-                                        key={item.id}
-                                        control={form.control}
-                                        name="modules"
-                                        render={({ field }) => {
-                                            return (
-                                                <FormItem
-                                                    key={item.id}
-                                                    className="flex flex-row items-center space-x-3 space-y-0"
-                                                >
-                                                    <FormControl>
-                                                        <Checkbox
-                                                            checked={field.value?.includes(
-                                                                item.id
-                                                            )}
-                                                            onCheckedChange={(
-                                                                checked
-                                                            ) => {
-                                                                return checked
-                                                                    ? field.onChange(
-                                                                          [
-                                                                              ...field.value,
-                                                                              item.id,
-                                                                          ]
-                                                                      )
-                                                                    : field.onChange(
-                                                                          field.value?.filter(
-                                                                              (
-                                                                                  value
-                                                                              ) =>
-                                                                                  value !==
-                                                                                  item.id
-                                                                          )
-                                                                      );
-                                                            }}
-                                                        />
-                                                    </FormControl>
-                                                    <FormLabel className="font-normal">
-                                                        {item.label}
-                                                    </FormLabel>
-                                                </FormItem>
-                                            );
+                                <FormControl>
+                                    <Input
+                                        // autoFocus
+                                        required
+                                        placeholder="e.g. John Doe"
+                                        {...field}
+                                        className="max-w-screen-sm h-10 bg-white"
+                                        autoComplete="on"
+                                        style={{
+                                            fontSize: 16,
                                         }}
                                     />
-                                ))}
+                                </FormControl>
                                 <FormMessage />
                             </FormItem>
                         )}
-                    /> */}
-          <FormField
-            control={form.control}
-            name="supervisee_name"
-            render={({ field }) => (
-              <FormItem className="w-full rounded-md">
-                <FormLabel className="flex flex-row gap-4 items-center">
-                  Logged in as
-                </FormLabel>
-                {/* <FormDescription>
-                            Give your newsletter a name that describes its
-                            content.
-                        </FormDescription> */}
-                <FormControl>
-                  <Input
+                    />
+                    {/* for child age */}
+                    <FormField
+                        control={form.control}
+                        name="supervisee_age"
+                        render={({ field }) => (
+                            <FormItem className="w-full rounded-md">
+                                <FormLabel className="flex flex-row gap-4 items-center">
+                                    {userFormAgeLabel}
+                                </FormLabel>
+                                <FormDescription>
+                                    {userFormAgeDescription}
+                                </FormDescription>
+                                <FormControl>
+                                    <Input
+                                        // autoFocus
+                                        required
+                                        placeholder="e.g. 8"
+                                        {...field}
+                                        className="max-w-screen-sm h-10 bg-white"
+                                        autoComplete="on"
+                                        style={{
+                                            fontSize: 16,
+                                        }}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="supervisee_persona"
+                        render={({ field }) => (
+                            <FormItem className="w-full rounded-md">
+                                <FormLabel className="flex flex-row gap-4 items-center">
+                                    {userFormPersonaLabel}
+                                </FormLabel>
+                                <FormControl>
+                                    <Textarea
+                                        rows={4}
+                                        placeholder="I like to play soccer, learn about new developments in economics and play the guitar. I also like being challenged."
+                                        {...field}
+                                        className="max-w-screen-sm bg-white"
+                                        autoComplete="on"
+                                        style={{
+                                            fontSize: 16,
+                                        }}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
+            <Separator className="mt-4 mb-6" />
+            <div className="flex flex-col gap-2">
+                <Label className="flex flex-row gap-4 items-center">
+                    Generate Starmoon API Key
+                </Label>
+                <div className="flex flex-row items-center gap-2 mt-2">
+                    <AuthTokenModal user={selectedUser} />
+                </div>
+                <p className="text-xs text-gray-400">
+                    This key must be kept secret and should not be shared.
+                </p>
+            </div>
+            <Separator className="mt-4 mb-6" />
+            <div className="flex flex-col gap-2">
+                <Label className="flex flex-row gap-4 items-center">
+                    Logged in as
+                </Label>
+                <Input
                     // autoFocus
                     disabled
                     value={selectedUser?.email}
                     className="max-w-screen-sm h-10 bg-white"
                     autoComplete="on"
                     style={{
-                      fontSize: 16,
+                        fontSize: 16,
                     }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-      <Separator className="mt-4 mb-6" />
-      <div className="flex flex-col gap-2">
-        <Label className="flex flex-row gap-4 items-center">
-          Generate Starmoon API Key
-        </Label>
-        <div className="flex flex-row items-center gap-2 mt-2">
-          <AuthTokenModal user={selectedUser} />
+                />
+            </div>
         </div>
-        <p className="text-xs text-gray-400">
-          This key must be kept secret and should not be shared.
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SettingsDashboard;
--- frontend/lib/data.ts
+++ frontend/lib/data.ts
@@ -2,6 +2,13 @@ export const defaultToyId: string = "56224f7f-250d-4351-84ee-e4a13b881c7b";
 export const defaultPersonalityId: string =
     "a1c073e6-653d-40cf-acc1-891331689409";
 
+export const userFormPersonaLabel =
+    "Briefly describe yourself and your interests, personality, and learning style";
+export const userFormAgeLabel = "Your age";
+export const userFormAgeDescription =
+    "Users under 13 years old must have a parent or guardian to setup Starmoon.";
+export const userFormNameLabel = "Your name";
+
 export const INITIAL_CREDITS = 50;
 export const SECONDS_PER_CREDIT = (30 * 60) / INITIAL_CREDITS; // 30 minutes equals 50 credits
 
