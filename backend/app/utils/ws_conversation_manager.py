2381c447abfe865ec0730fae4d42081b43117fde
Joey Xiong <junruxiong@gmail.com>
2024-09-15 02:59:24 +0100

    add fastAPI replicas

--- backend/app/utils/ws_conversation_manager.py
+++ backend/app/utils/ws_conversation_manager.py
@@ -114,10 +114,6 @@ class ConversationManager:
                 # Cancel all check_task_result tasks
                 for task in self.check_task_result_tasks:
                     task.cancel()
-                    try:
-                        await task
-                    except asyncio.CancelledError:
-                        pass
                 self.check_task_result_tasks.clear()
                 break
             if self.connection_open == False:
@@ -304,16 +300,7 @@ class ConversationManager:
                             break
 
                     transcription_task.cancel()
-                    try:
-                        await transcription_task
-                    except asyncio.CancelledError:
-                        pass
-
                     timeout_task.cancel()
-                    try:
-                        await timeout_task
-                    except asyncio.CancelledError:
-                        pass
 
                     if not self.connection_open:
                         break
