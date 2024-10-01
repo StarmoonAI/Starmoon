21e25b447fce8084ebd60d59fe88830080137a92
akdeb <deb2@illinois.edu>
2024-10-01 10:43:49 +0100

    minor change

--- frontend/app/components/NavbarButtons.tsx
+++ frontend/app/components/NavbarButtons.tsx
@@ -5,37 +5,37 @@ import { LogIn, Home } from "lucide-react";
 import { Gamepad2, ShoppingCart } from "lucide-react";
 
 interface NavbarButtonsProps {
-  user: User | null;
+    user: User | null;
 }
 
 const ICON_SIZE = 18;
 const STROKE_WIDTH = 3;
 
 const NavbarButtons: React.FC<NavbarButtonsProps> = ({ user }) => {
-  return user ? (
-    <Link href="/home">
-      <Button
-        variant="secondary"
-        size="sm"
-        className="flex flex-row items-center gap-2 rounded-full bg-nav-bar"
-      >
-        <Home size={ICON_SIZE} />
-        <span className="hidden md:inline">Home</span>
-      </Button>
-    </Link>
-  ) : (
-    <Link href="/login">
-      <Button
-        variant="secondary"
-        size="sm"
-        className="font-medium flex flex-row items-center gap-2 rounded-full bg-nav-bar"
-      >
-        {/* <LogIn size={ICON_SIZE} strokeWidth={STROKE_WIDTH} /> */}
-        <Gamepad2 size={22} />
-        <span className="hidden sm:block">Play Online</span>
-      </Button>
-    </Link>
-  );
+    return user ? (
+        <Link href="/home">
+            <Button
+                variant="secondary"
+                size="sm"
+                className="flex flex-row items-center gap-2 rounded-full bg-nav-bar"
+            >
+                <Home size={ICON_SIZE} />
+                <span className="hidden sm:block">Home</span>
+            </Button>
+        </Link>
+    ) : (
+        <Link href="/login">
+            <Button
+                variant="secondary"
+                size="sm"
+                className="font-medium flex flex-row items-center gap-2 rounded-full bg-nav-bar"
+            >
+                {/* <LogIn size={ICON_SIZE} strokeWidth={STROKE_WIDTH} /> */}
+                <Gamepad2 size={22} />
+                <span className="hidden sm:block">Play Online</span>
+            </Button>
+        </Link>
+    );
 };
 
 export default NavbarButtons;
